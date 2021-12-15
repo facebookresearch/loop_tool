caa8f9deedd2220069d6154ecc1dfc37c38fee96
Bram Wasti <bwasti@fb.com>
2021-12-15 11:02:03 -0800

    run black for reformatting

--- test/test.py
+++ test/test.py
@@ -214,35 +214,35 @@ def test_annot(N, M):
     #   3. disable reuse (discrete)
     #   3. thread (discrete)
 
-    ir.set_order(r, [n : Var, 8, 0, m : Var, 7, 0])
-    for n in 8:
-      for m in 7:
-        r[n * 7 + m]
-    ir.set_order(r, [n : Var, 2, 2, m : Var, 7, 0, n : Var, 3, 0])
-    for n in 2 r 2:
-      for m in 7:
-        for n_1 in 3:
-          r[(n * 3 + n_1) * 7 + m]
+    # ir.set_order(r, [n : Var, 8, 0, m : Var, 7, 0])
+    # for n in 8:
+    #  for m in 7:
+    #    r[n * 7 + m]
+    # ir.set_order(r, [n : Var, 2, 2, m : Var, 7, 0, n : Var, 3, 0])
+    # for n in 2 r 2:
+    #  for m in 7:
+    #    for n_1 in 3:
+    #      r[(n * 3 + n_1) * 7 + m]
 
-    === 
-    [1, 2, 4, 5, 6]
-    [1, 2, (4, 5, 6)]
-    [1, 2, (5, 4, 6)]
-    [1, (2, 5, 4), 6]
-    [1, (5, 2, 4), 6]
-    [(1, 5, 2), 4, 6]
+    # ===
+    # [1, 2, 4, 5, 6]
+    # [1, 2, (4, 5, 6)]
+    # [1, 2, (5, 4, 6)]
+    # [1, (2, 5, 4), 6]
+    # [1, (5, 2, 4), 6]
+    # [(1, 5, 2), 4, 6]
 
-    for n in 2:
-      for m in 7:
-        for n_1 in 3:
-          r[(n * 3 + n_1) * 7 + m]
-    n = 2
-      for m in 7:
-        for n_1 in 2:
-          r[(n * 3 + n_1) * 7 + m]
+    # for n in 2:
+    #  for m in 7:
+    #    for n_1 in 3:
+    #      r[(n * 3 + n_1) * 7 + m]
+    # n = 2
+    #  for m in 7:
+    #    for n_1 in 2:
+    #      r[(n * 3 + n_1) * 7 + m]
 
-    #ir.disable_reuse(r)
-    #ir.thread(r)
+    # ir.disable_reuse(r)
+    # ir.thread(r)
 
     ir.set_order(add0, [(n, (N, 0)), (m, (M, 0))])
     ir.disable_reuse(r, m)
--- test/test_views.py
+++ test/test_views.py
@@ -1,80 +1,87 @@
 import loop_tool_py as lt
 import numpy as np
 
+
 def test_pad():
-  m, n = 128, 16
+    m, n = 128, 16
 
-  base_np = np.random.randn(m, n)
-  padded_np = np.pad(base_np, [(0,), (3,)])
+    base_np = np.random.randn(m, n)
+    padded_np = np.pad(base_np, [(0,), (3,)])
 
-  base_lt = lt.Tensor(m, n).set(base_np)
-  padded_lt = base_lt.pad(base_lt.symbolic_shape[1], 3)
+    base_lt = lt.Tensor(m, n).set(base_np)
+    padded_lt = base_lt.pad(base_lt.symbolic_shape[1], 3)
 
-  assert np.allclose(padded_lt.numpy(), padded_np)
+    assert np.allclose(padded_lt.numpy(), padded_np)
+
 
 def test_concat():
-  m, n, k = 128, 16, 5
+    m, n, k = 128, 16, 5
 
-  A_np = np.random.randn(m, n)
-  B_np = np.random.randn(m, k)
-  C_np = np.concatenate((A_np, B_np), axis=1)
+    A_np = np.random.randn(m, n)
+    B_np = np.random.randn(m, k)
+    C_np = np.concatenate((A_np, B_np), axis=1)
 
-  A_lt = lt.Tensor(m, n).set(A_np)
-  B_lt = lt.Tensor(m, k).set(B_np)
+    A_lt = lt.Tensor(m, n).set(A_np)
+    B_lt = lt.Tensor(m, k).set(B_np)
 
-  with lt.SymbolGenerator() as s:
-    C_lt = A_lt.to(s.m, s.n) | B_lt.to(s.m, s.k)
+    with lt.SymbolGenerator() as s:
+        C_lt = A_lt.to(s.m, s.n) | B_lt.to(s.m, s.k)
 
-  assert np.allclose(C_lt.numpy(), C_np)
+    assert np.allclose(C_lt.numpy(), C_np)
+
 
 def test_2d_conv():
-  import torch
-  import torch.nn.functional as F
+    import torch
+    import torch.nn.functional as F
 
-  def conv2d(X, W):
-      s = lt.SymbolGenerator()
-      X = X[s.C, s.H + s.Kh, s.W + s.Kw]
-      W = W.to(s.Co, s.C, s.Kh, s.Kw)
-      return (X * W).sum(s.C, s.Kh, s.Kw).transpose(s.Co, s.H, s.W)
+    def conv2d(X, W):
+        s = lt.SymbolGenerator()
+        X = X[s.C, s.H + s.Kh, s.W + s.Kw]
+        W = W.to(s.Co, s.C, s.Kh, s.Kw)
+        return (X * W).sum(s.C, s.Kh, s.Kw).transpose(s.Co, s.H, s.W)
 
-  ci = 16
-  co = 16
-  x = 8
-  k = 3
-  X_np = np.random.randn(ci, x, x)
-  W_np = np.random.randn(co, ci, k, k)
-  Y_np = F.conv2d(torch.tensor(X_np).unsqueeze(0), torch.tensor(W_np)).numpy()
+    ci = 16
+    co = 16
+    x = 8
+    k = 3
+    X_np = np.random.randn(ci, x, x)
+    W_np = np.random.randn(co, ci, k, k)
+    Y_np = F.conv2d(torch.tensor(X_np).unsqueeze(0), torch.tensor(W_np)).numpy()
 
-  X_lt = lt.Tensor(ci, x, x).set(X_np)
-  W_lt = lt.Tensor(co, ci, k, k).set(W_np)
-  Y_lt = conv2d(X_lt, W_lt)
+    X_lt = lt.Tensor(ci, x, x).set(X_np)
+    W_lt = lt.Tensor(co, ci, k, k).set(W_np)
+    Y_lt = conv2d(X_lt, W_lt)
 
-  assert np.allclose(Y_lt.numpy(), Y_np, rtol=0.001, atol=0.001)
+    assert np.allclose(Y_lt.numpy(), Y_np, rtol=0.001, atol=0.001)
+
 
 def test_padded_2d_conv():
-  import torch
-  import torch.nn.functional as F
+    import torch
+    import torch.nn.functional as F
 
-  def conv2d(X, W):
-      s = lt.SymbolGenerator()
-      X = X.to(s.c, s.h, s.w).pad(s.h, 1).pad(s.w, 1)
-      X = X[s.C, s.H + s.Kh, s.W + s.Kw]
-      W = W.to(s.Co, s.C, s.Kh, s.Kw)
-      return (X * W).sum(s.C, s.Kh, s.Kw).transpose(s.Co, s.H, s.W)
+    def conv2d(X, W):
+        s = lt.SymbolGenerator()
+        X = X.to(s.c, s.h, s.w).pad(s.h, 1).pad(s.w, 1)
+        X = X[s.C, s.H + s.Kh, s.W + s.Kw]
+        W = W.to(s.Co, s.C, s.Kh, s.Kw)
+        return (X * W).sum(s.C, s.Kh, s.Kw).transpose(s.Co, s.H, s.W)
 
-  ci = 16
-  co = 16
-  x = 8
-  k = 3
-  X_np = np.random.randn(ci, x, x)
-  W_np = np.random.randn(co, ci, k, k)
-  Y_np = F.conv2d(torch.tensor(X_np).unsqueeze(0), torch.tensor(W_np), padding=1).numpy()
+    ci = 16
+    co = 16
+    x = 8
+    k = 3
+    X_np = np.random.randn(ci, x, x)
+    W_np = np.random.randn(co, ci, k, k)
+    Y_np = F.conv2d(
+        torch.tensor(X_np).unsqueeze(0), torch.tensor(W_np), padding=1
+    ).numpy()
 
-  X_lt = lt.Tensor(ci, x, x).set(X_np)
-  W_lt = lt.Tensor(co, ci, k, k).set(W_np)
-  Y_lt = conv2d(X_lt, W_lt)
+    X_lt = lt.Tensor(ci, x, x).set(X_np)
+    W_lt = lt.Tensor(co, ci, k, k).set(W_np)
+    Y_lt = conv2d(X_lt, W_lt)
 
-  assert np.allclose(Y_lt.numpy(), Y_np, rtol=0.001, atol=0.001)
+    assert np.allclose(Y_lt.numpy(), Y_np, rtol=0.001, atol=0.001)
+
 
 test_pad()
 test_concat()
