d8a4e9030457e275998fc58b2151c63871c2424d
Bram Wasti <bwasti@brams-air.myfiosgateway.com>
2022-02-11 23:17:44 -0500

    fix apple silicon build

--- install_cuda.sh
+++ install_cuda.sh
@@ -5,18 +5,9 @@ cat /proc/version
 
 apt-get -qq update
 apt-get -yqq install wget libxml2-dev
-wget -q https://developer.download.nvidia.com/compute/cuda/11.4.2/local_installers/cuda_11.4.2_470.57.02_linux.run
-sh cuda_11.4.2_470.57.02_linux.run --help
-sh cuda_11.4.2_470.57.02_linux.run --silent --toolkit
+wget -q https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_510.39.01_linux.run
+sh cuda_11.6.0_510.39.01_linux.run --help
+sh cuda_11.6.0_510.39.01_linux.run --silent --toolkit
 cat /var/log/cuda-installer.log
-
-#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-#mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#wget -q https://developer.download.nvidia.com/compute/cuda/11.4.2/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.2-470.57.02-1_amd64.deb
-#dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.2-470.57.02-1_amd64.deb
-#APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
-#apt-get update
-#apt-get -yqq install cuda
-#apt-get update
 
 ls /usr/local/cuda
